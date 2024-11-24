e37078104c657e88b73ab3de9c718e91334f5db6
Lup Yuen Lee <luppy@appkaki.com>
2024-11-24 18:07:09 +0800

    Adding rewind build

--- rewind-commit.sh
+++ rewind-commit.sh
@@ -17,43 +17,33 @@ if [[ "$target" == "" ]]; then
   exit 1
 fi
 
-## Second Parameter is the Timestamp of the NuttX Repo, like "2024-11-24T00:00:00"
-timestamp=$2
-if [[ "$timestamp" == "" ]]; then
-  echo "ERROR: Timestamp is missing (e.g. ox64:nsh)"
-  exit 1
-fi
-
-## Third Parameter is the Commit Hash of NuttX Apps Repo, like "d6edbd0cec72cb44ceb9d0f5b932cbd7a2b96288"
-apps_hash=$3
+## Second Parameter is the Commit Hash of NuttX Apps Repo, like "d6edbd0cec72cb44ceb9d0f5b932cbd7a2b96288"
+apps_hash=$2
 if [[ "$apps_hash" == "" ]]; then
   echo "ERROR: NuttX Apps Hash is missing (e.g. d6edbd0cec72cb44ceb9d0f5b932cbd7a2b96288)"
   exit 1
 fi
 
-## Fourth Parameter is the Commit Hash of NuttX Repo, like "7f84a64109f94787d92c2f44465e43fde6f3d28f"
-nuttx_hash=$4
+## Third Parameter is the Commit Hash of NuttX Repo, like "7f84a64109f94787d92c2f44465e43fde6f3d28f"
+nuttx_hash=$3
 if [[ "$nuttx_hash" == "" ]]; then
   echo "ERROR: NuttX Commit Hash is missing (e.g. 7f84a64109f94787d92c2f44465e43fde6f3d28f)"
   exit 1
 fi
 
-## Fifth Parameter is the Previous Commit Hash of NuttX Repo, like "7f84a64109f94787d92c2f44465e43fde6f3d28f"
-prev_hash=$5
+## Fourth Parameter is the Previous Commit Hash of NuttX Repo, like "7f84a64109f94787d92c2f44465e43fde6f3d28f"
+prev_hash=$4
 if [[ "$prev_hash" == "" ]]; then
   echo "ERROR: Previous NuttX Commit Hash is missing (e.g. 7f84a64109f94787d92c2f44465e43fde6f3d28f)"
   exit 1
 fi
 
-## Sixth Parameter is the Next Commit Hash of NuttX Repo, like "7f84a64109f94787d92c2f44465e43fde6f3d28f"
-next_hash=$6
+## Fifth Parameter is the Next Commit Hash of NuttX Repo, like "7f84a64109f94787d92c2f44465e43fde6f3d28f"
+next_hash=$5
 if [[ "$next_hash" == "" ]]; then
   echo "ERROR: Next NuttX Commit Hash is missing (e.g. 7f84a64109f94787d92c2f44465e43fde6f3d28f)"
   exit 1
 fi
-
-tmp_dir=/tmp/rewind-build/$target/$nuttx_hash
-####log_file=/tmp/release-$device.log
 
 ## Get the Script Directory
 script_path="${BASH_SOURCE}"
@@ -79,8 +69,9 @@ sleep 10
 ## Build NuttX in Docker Container
 ## If CI Test Hangs: Kill it after 1 hour
 function build_nuttx {
-  nuttx_commit=$1
-  apps_commit=$2
+  local nuttx_commit=$1
+  local apps_commit=$2
+  set +e  ## Ignore errors
   sudo docker run -it \
     ghcr.io/apache/nuttx/apache-nuttx-ci-linux:latest \
     /bin/bash -c "
@@ -102,14 +93,18 @@ function build_nuttx {
       (./tools.configure $target && make -j) || (echo '***** BUILD FAILED' ; exit 1)
     );
   "
+  local res=$?
+  set -e  ## Exit when any command fails
+  echo res=$res
 }
 
 ## Build the Target for the Commit
-build_nuttx nuttx_hash apps_hash
+res=build_nuttx nuttx_hash apps_hash
+echo res=$res
 
 ## If it fails: Rebuild with Previous Commit and Next Commit
-build_nuttx prev_hash apps_hash
-build_nuttx next_hash apps_hash
+# build_nuttx prev_hash apps_hash
+# build_nuttx next_hash apps_hash
 
 ## Monitor the Disk Space (in case Docker takes too much)
 df -H
