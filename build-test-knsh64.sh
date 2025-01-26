ed97d85c8b7e1267e79cf8ba6033324952a97e63
Lup Yuen Lee <luppy@appkaki.com>
2025-01-26 16:11:33 +0800

    Build and test knsh64

---
+++ build-test-knsh64.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+## Build and Test NuttX for QEMU RISC-V 64-bit (Kernel Build)
+echo "***** Now running https://github.com/lupyuen/nuttx-build-farm/blob/main/build-test-knsh64.sh $1 $2"
+
+set -e  #  Exit when any command fails
+set -x  #  Echo commands
+
+nuttx_hash=$1  ## Optional NuttX Hash
+apps_hash=$2  ## Optional Apps Hash
+tmp_path=/tmp/build-test-knsh64
+rm -rf $tmp_path
+mkdir $tmp_path
+cd $tmp_path
+
+## TODO: neofetch
+
+wget https://sourceforge.net/projects/expect/files/Expect/5.45.4/expect5.45.4.tar.gz/download
+mv download expect5.45.4.tar.gz
+tar xvf expect5.45.4.tar.gz
+
+wget https://core.tcl-lang.org/tcl/tarball/release/tcl.tar.gz
+tar xvf tcl.tar.gz
+
+pushd tcl/unix
+./configure
+make -j
+make install
+popd
+
+pushd expect5.45.4
+./configure
+make -j
+make install
+popd
+
+## Download NuttX and Apps
+git clone https://github.com/apache/nuttx
+git clone https://github.com/apache/nuttx-apps apps
+
+## Switch to this NuttX Commit
+if [[ "$nuttx_hash" != "" ]]; then
+  pushd nuttx
+  git reset --hard $nuttx_hash
+  popd
+fi
+
+## Switch to this Apps Commit
+if [[ "$apps_hash" != "" ]]; then
+  pushd apps
+  git reset --hard $apps_hash
+  popd
+fi
+
+## Dump the git hash
+pushd nuttx ; echo NuttX Source: https://github.com/apache/nuttx/tree/$(git rev-parse HEAD) ; popd
+pushd apps  ; echo NuttX Apps: https://github.com/apache/nuttx-apps/tree/$(git rev-parse HEAD) ; popd
+
+## Show the GCC and Rust versions
+riscv-none-elf-gcc -v
+rustup --version || true
+rustc  --version || true
+
+## Configure the build
+cd nuttx
+tools/configure.sh rv-virt:knsh64
+
+## Preserve the build config
+cp .config nuttx.config
+
+## Run the build
+make -j
+
+## Build Apps Filesystem
+make export
+pushd ../apps
+./tools/mkimport.sh -z -x ../nuttx/nuttx-export-*.tar.gz
+make import
+popd
+
+## Show the size
+riscv-none-elf-size nuttx
+
+## Run the test
+qemu-system-riscv64 --version
+script=qemu-riscv-knsh64
+wget https://raw.githubusercontent.com/lupyuen/nuttx-riscv64/main/$script.exp
+chmod +x $script.exp
+./$script.exp
