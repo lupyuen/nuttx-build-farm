84c5f5fed41a48aa81305485b59b51457a2dbf23
Lup Yuen Lee <luppy@appkaki.com>
2025-02-06 19:34:27 +0800

    Clean up

--- oz64-power.sh
+++ oz64-power.sh
@@ -5,7 +5,6 @@
 echo "Now running https://github.com/lupyuen/nuttx-build-farm/blob/main/oz64-power.sh $1"
 
 set -e  ## Exit when any command fails
-set -x  ## Echo commands
 
 ## First Parameter is on or off
 state=$1
@@ -16,14 +15,11 @@ fi
 
 ## Get the Home Assistant Token, copied from http://localhost:8123/profile/security
 ## export HOME_ASSISTANT_TOKEN=xxxx
-set +x  ##  Disable echo
 . $HOME/home-assistant-token.sh
-set -x  ##  Enable echo
 
 ## Set the Home Assistant Server
 export HOME_ASSISTANT_SERVER=luppys-mac-mini.local:8123
 
-set +x  ##  Disable echo
 echo "----- Power $state Oz64"
 curl \
     -X POST \
@@ -31,4 +27,3 @@ curl \
     -H "Content-Type: application/json" \
     -d "{\"entity_id\": \"automation.oz64_power_$state\"}" \
     http://$HOME_ASSISTANT_SERVER/api/services/automation/trigger
-set -x  ##  Enable echo
