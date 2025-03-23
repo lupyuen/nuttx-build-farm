cd56fda2b4339385a106d512a816133f8ffbde9c
Lup Yuen Lee <luppy@appkaki.com>
2025-03-23 09:40:20 +0800

    Daily Build and Test for Avaota-A1 and StarPro64
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- daily-test.sh
+++ daily-test.sh
@@ -4,6 +4,8 @@
 ## Daily Build and Test for Avaota-A1 and StarPro64: Every Day at 00:00 UTC
 ## 0 0 * * * /home/luppy/nuttx-build-farm/daily-test.sh 2>&1 | logger -t nuttx-daily-test
 
+set -e  #  Exit when any command fails
+
 ## Set the GitHub Token
 ## export GITHUB_TOKEN=...
 . $HOME/github-token.sh
@@ -13,13 +15,16 @@ export PATH=$HOME/xpack-riscv-none-elf-gcc-13.2.0-2/bin:$PATH
 export PATH=$HOME/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/bin:$PATH
 . "$HOME/.cargo/env"
 
-set -e  #  Exit when any command fails
 set -x  #  Echo commands
+
+## Get the Script Directory
+script_path="${BASH_SOURCE}"
+script_dir="$(cd -P "$(dirname -- "${script_path}")" >/dev/null 2>&1 && pwd)"
 
 ## Build and Test for Avaota-A1
 script=avaota
 log_file=/tmp/daily-test-$script
-./build-test.sh $script $log_file
+$script_dir/build-test.sh $script $log_file
 cat $log_file | \
   gh gist create \
   --public \
@@ -29,7 +34,7 @@ cat $log_file | \
 ## Build and Test for StarPro64
 script=starpro64
 log_file=/tmp/daily-test-$script
-./build-test.sh $script $log_file
+$script_dir/build-test.sh $script $log_file
 cat $log_file | \
   gh gist create \
   --public \
